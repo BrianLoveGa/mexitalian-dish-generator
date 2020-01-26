7f48696fe59bfbb2959b7b0c78ceb316370b1491
Brian Loveless <Brian.loveless1@yahoo.com>
2020-01-26 18:31:55 -0500

    dont forget your veggies

--- work.js
+++ work.js
@@ -16,6 +16,7 @@ let meat = document.querySelector(".meat");
 let starch = document.querySelector(".starch");
 let sauce = document.querySelector(".sauce");
 let cheese = document.querySelector(".cheese");
+let veggies = document.querySelector(".veggies");
 
 // set it to a blank to start
 
@@ -23,36 +24,109 @@ meat.innerHTML = "";
 starch.innerHTML = "";
 sauce.innerHTML = "";
 cheese.innerHTML = "";
+veggies.innerHTML = "";
 
 // grab the buttons - give em functions work from bottom up
-// start with change the cheese cycle through only the italian options
-const itCheese = document.getElementById("itlCheese");
-itCheese.addEventListener("click", rndmITLCheese);
+// start with italian veggies
 
-function rndmITLCheese() {
+const itlVeg = document.getElementById("itl-vegs");
+itlVeg.addEventListener("click", rndmItlVeg);
+
+function rndmItlVeg() {
+  veggies.innerHTML = itlRndmVeg();
+}
+
+function itlRndmVeg() {
+  switch (Math.floor(Math.random() * 6)) {
+    case 0:
+      return "broccoli and mushrooms";
+    case 1:
+      return "mushrooms onions peppers";
+    case 2:
+      return "peas and carrots";
+    case 3:
+      return "roasted zuchinni and cauliflower";
+    case 4:
+      return "garlic artichoke hearts";
+    case 5:
+      return "olives celery and fennel";
+  }
+}
+
+// now mex vegs
+const mexVeg = document.getElementById("mex-vegs");
+mexVeg.addEventListener("click", rndmMexVeg);
+
+function rndmMexVeg() {
+  veggies.innerHTML = mexRndmVeg();
+}
+
+function mexRndmVeg() {
+  switch (Math.floor(Math.random() * 8)) {
+    case 0:
+      return "tomatoes and tomatillos";
+    case 1:
+      return "jicama";
+    case 2:
+      return "chayote";
+    case 3:
+      return "spinach and cauliflower";
+    case 4:
+      return "mushrooms and zuchini";
+    case 5:
+      return "poblano and red chili peppers";
+    case 6:
+      return "green bell and jalapeño peppers";
+    case 7:
+      return "shredded lettuce, fresh mango salsa ";
+  }
+}
+
+// now for a random from either for mex or itl for veggies
+// lets give it a 50 50 shot of a random mex veg or itl veg
+// button click
+const allVeg = document.getElementById("all-vegs");
+// add function
+allVeg.addEventListener("click", randoVeg);
+// make function
+
+function randoVeg() {
+  switch (Math.floor(Math.random() * 2)) {
+    case 0:
+      return rndmItlVeg();
+    case 1:
+      return rndmMexVeg();
+  }
+}
+
+// now next we have cheese to cycle through starting with only the italian options
+const itlCheese = document.getElementById("itlCheese");
+itlCheese.addEventListener("click", rndmItlCheese);
+
+function rndmItlCheese() {
   cheese.innerHTML = itlRndmCheese();
 }
 
 function itlRndmCheese() {
   switch (Math.floor(Math.random() * 9)) {
     case 0:
-      return "Gorgonzola";
+      return "gorgonzola";
     case 1:
-      return "Pecorino Toscano";
+      return "pecorino Toscano";
     case 2:
-      return "Taleggio";
+      return "taleggio";
     case 3:
-      return "Fontina d'Aosta";
+      return "fontina d'Aosta";
     case 4:
-      return "Parmigiano-Reggiano";
+      return "parmigiano-Reggiano";
     case 5:
-      return "Mozzarella di Bufala";
+      return "mozzarella di Bufala";
     case 6:
-      return "Provolone";
+      return "provolone";
     case 7:
-      return "Robiola Peimonte";
+      return "robiola peimonte";
     case 8:
-      return "Asiago";
+      return "asiago";
   }
 }
 // start with change the cheese cycle through only the mexican options
@@ -66,23 +140,23 @@ function rndmMEXCheese() {
 function mexRndmCheese() {
   switch (Math.floor(Math.random() * 9)) {
     case 0:
-      return "Queso Añejo";
+      return "queso añejo";
     case 1:
-      return "Queso Fresco";
+      return "queso fresco";
     case 2:
-      return "Cotija";
+      return "cotija";
     case 3:
-      return "Queso Panela";
+      return "queso panela";
     case 4:
-      return "Asadero";
+      return "asadero";
     case 5:
-      return "Queso De Oxaca";
+      return "queso de oxaca";
     case 6:
-      return "Queso Blanco";
+      return "queso blanco";
     case 7:
-      return "Requeson";
+      return "requeson";
     case 8:
-      return "Crema Mexicana";
+      return "queso chihuahua";
   }
 }
 // use the previous functions to pick mexican or italian cheese
@@ -94,37 +168,12 @@ function randoCheese() {
     case 0:
       return rndmMEXCheese();
     case 1:
-      return rndmITLCheese();
+      return rndmItlCheese();
   }
 }
 
 // now for the sauces
-// again we will start south with mexican ones
-const mexSauce = document.getElementById("mex-sauce");
-mexSauce.addEventListener("click", rndmMexSauce);
-
-function rndmMexSauce() {
-  sauce.innerHTML = mexRndmSauce();
-}
-
-function mexRndmSauce() {
-  switch (Math.floor(Math.random() * 6)) {
-    case 0:
-      return "Guacamole";
-    case 1:
-      return "Pico de gallo";
-    case 2:
-      return "Red Salsa";
-    case 3:
-      return "Salsa Verde";
-    case 4:
-      return "Green Chili";
-    case 5:
-      return "Enchilada sauce";
-  }
-}
-
-// now italian sauce
+// again we will start  with italian ones
 const itlSauce = document.getElementById("itl-sauce");
 itlSauce.addEventListener("click", rndmItlSauce);
 
@@ -148,6 +197,31 @@ function itlRndmSauce() {
       return "Red Clam sauce";
   }
 }
+// now the mexican sauces
+
+const mexSauce = document.getElementById("mex-sauce");
+mexSauce.addEventListener("click", rndmMexSauce);
+
+function rndmMexSauce() {
+  sauce.innerHTML = mexRndmSauce();
+}
+
+function mexRndmSauce() {
+  switch (Math.floor(Math.random() * 6)) {
+    case 0:
+      return "guacamole";
+    case 1:
+      return "pico de gallo";
+    case 2:
+      return "red salsa";
+    case 3:
+      return "salsa verde";
+    case 4:
+      return "green chili";
+    case 5:
+      return "enchilada sauce";
+  }
+}
 
 // now all sauces together
 
@@ -165,32 +239,7 @@ function randoSauce() {
 
 // now starches
 
-const mexStarch = document.getElementById("mex-starch");
-mexStarch.addEventListener("click", rndmMexStarch);
-
-function rndmMexStarch() {
-  starch.innerHTML = mexRndmStarch();
-}
-
-function mexRndmStarch() {
-  switch (Math.floor(Math.random() * 6)) {
-    case 0:
-      return "on top of hard flour tortillas";
-    case 1:
-      return "on top of soft corn tortillas";
-    case 2:
-      return "on top of soft flour tortillas";
-    case 3:
-      return "on top of hard corn tortillas";
-    case 4:
-      return "rolled inside of soft flour tortillas";
-    case 5:
-      return "rolled inside of soft corn tortillas";
-  }
-}
-
-// italian pasta / starch
-
+// pasta
 const itlStarch = document.getElementById("itl-starch");
 itlStarch.addEventListener("click", rndmItlStarch);
 
@@ -214,6 +263,30 @@ function itlRndmStarch() {
       return "on top of fusilli lunghi noodles";
   }
 }
+// tortilla
+const mexStarch = document.getElementById("mex-starch");
+mexStarch.addEventListener("click", rndmMexStarch);
+
+function rndmMexStarch() {
+  starch.innerHTML = mexRndmStarch();
+}
+
+function mexRndmStarch() {
+  switch (Math.floor(Math.random() * 6)) {
+    case 0:
+      return "on top of hard flour tortilla chips";
+    case 1:
+      return "on top of soft corn tortillas";
+    case 2:
+      return "on top of soft flour tortillas";
+    case 3:
+      return "on top of soft wheat tortillas";
+    case 4:
+      return "rolled inside of soft flour tortillas";
+    case 5:
+      return "rolled inside of blue corn tortillas";
+  }
+}
 
 // and now both starches together
 const allStarch = document.getElementById("all-starch");
@@ -228,34 +301,6 @@ function randoStarch() {
   }
 }
 // DA MEATS
-
-const mexMeat = document.getElementById("mex-meat");
-mexMeat.addEventListener("click", rndmMexMeat);
-
-function rndmMexMeat() {
-  meat.innerHTML = mexRndmMeat();
-}
-
-function mexRndmMeat() {
-  switch (Math.floor(Math.random() * 8)) {
-    case 0:
-      return "fried pulled pork";
-    case 1:
-      return "finely chopped beef steak";
-    case 2:
-      return "adobe chicken";
-    case 3:
-      return "carne asada";
-    case 4:
-      return "carnitas";
-    case 5:
-      return "chorizo";
-    case 6:
-      return "al pastor";
-    case 7:
-      return "barbacoa";
-  }
-}
 
 // italian meat
 const itlMeat = document.getElementById("itl-meat");
@@ -286,6 +331,34 @@ function itlRndmMeat() {
   }
 }
 
+// mex meat
+const mexMeat = document.getElementById("mex-meat");
+mexMeat.addEventListener("click", rndmMexMeat);
+
+function rndmMexMeat() {
+  meat.innerHTML = mexRndmMeat();
+}
+
+function mexRndmMeat() {
+  switch (Math.floor(Math.random() * 8)) {
+    case 0:
+      return "fried pulled pork";
+    case 1:
+      return "finely chopped beef steak";
+    case 2:
+      return "adobe chicken";
+    case 3:
+      return "carne asada";
+    case 4:
+      return "carnitas";
+    case 5:
+      return "chorizo";
+    case 6:
+      return "al pastor";
+    case 7:
+      return "barbacoa";
+  }
+}
 // rando meat - how often do you read that in code?
 
 const allMeat = document.getElementById("all-meat");
@@ -300,6 +373,20 @@ function randoMeat() {
   }
 }
 
+// now individual ingredients done lets go big picture
+// seperate just italian ingrediants
+
+const onlyItl = document.getElementById("all-itl");
+onlyItl.addEventListener("click", allItlFood);
+
+function allItlFood() {
+  rndmItlCheese();
+  rndmItlSauce();
+  rndmItlStarch();
+  rndmItlMeat();
+  rndmItlVeg();
+}
+
 // seperate just mexican ingredients
 const onlyMex = document.getElementById("all-mex");
 onlyMex.addEventListener("click", allMexFood);
@@ -309,21 +396,10 @@ function allMexFood() {
   rndmMexSauce();
   rndmMexStarch();
   rndmMexMeat();
+  rndmMexVeg();
 }
 
-// seperate just italian ingrediants
-
-const onlyItl = document.getElementById("all-itl");
-onlyItl.addEventListener("click", allItlFood);
-
-function allItlFood() {
-  rndmITLCheese();
-  rndmItlSauce();
-  rndmItlStarch();
-  rndmItlMeat();
-}
-
-// and finally all ingredients all together and randomized
+// and finally all ingredients all together and 'randomized'
 const allDish = document.getElementById("all");
 allDish.addEventListener("click", fixMyDish);
 //  since we can't return multiple functions I'll divide up outcomes below
@@ -358,53 +434,62 @@ function rndmOne() {
   rndmItlSauce();
   rndmItlStarch();
   rndmItlMeat();
+  rndmItlVeg();
 }
 function rndmTwo() {
   rndmMEXCheese();
   rndmMexSauce();
   rndmItlStarch();
   rndmItlMeat();
+  rndmMexVeg();
 }
 function rndmThree() {
   rndmMEXCheese();
   rndmMexSauce();
   rndmMexStarch();
   rndmItlMeat();
+  rndmItlVeg();
 }
 function rndmFour() {
-  rndmITLCheese();
+  rndmItlCheese();
   rndmMexStarch();
   rndmItlSauce();
   rndmItlMeat();
+  rndmMexVeg();
 }
 function rndmFive() {
   rndmMexStarch();
   rndmMexMeat();
-  rndmITLCheese();
+  rndmItlCheese();
   rndmItlSauce();
+  rndmItlVeg();
 }
 
 function rndmSix() {
   rndmMexSauce();
-  rndmITLCheese();
+  rndmItlCheese();
   rndmItlStarch();
   rndmItlMeat();
+  rndmMexVeg();
 }
 function rndmSeven() {
   rndmMEXCheese();
   rndmMexSauce();
   rndmMexStarch();
   rndmItlMeat();
+  rndmItlVeg();
 }
 function rndmEight() {
   rndmMexSauce();
   rndmMexStarch();
-  rndmITLCheese();
+  rndmItlCheese();
   rndmItlMeat();
+  rndmMexVeg();
 }
 function rndmNine() {
   rndmMEXCheese();
   rndmMexMeat();
   rndmItlSauce();
   rndmItlStarch();
+  rndmItlVeg();
 }
